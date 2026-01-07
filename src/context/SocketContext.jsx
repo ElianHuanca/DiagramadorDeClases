827b8e2b9b247a252c45fb945c48a4c3da0c2250
ElianHuanca <huancacori@gmail.com>
2026-01-06 20:38:31 -0400

    agregando configuracion a nginx

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -7,7 +7,7 @@ export const SocketProvider = ({ children }) => {
     //const url = import.meta.env.VITE_API_URL;
     const url = window.__ENV__?.API_URL
         ? `${window.__ENV__.API_URL}`
-        : `${window.location.origin}/socket.io`;
+        : `${window.location.origin}/socket.io/`;
     console.log("Socket URL:", url);
     const { socket, online } = useSocket(url);
 
